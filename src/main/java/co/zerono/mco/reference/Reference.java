194b068352a7918288b884470ccefcdfb1333589
ZeronoAshaen <zeronoashaen@gmail.com>
2015-03-23 21:05:33 -0500

    Setup for first build

--- src/main/java/co/zerono/mco/reference/Reference.java
+++ src/main/java/co/zerono/mco/reference/Reference.java
@@ -13,7 +13,7 @@ public class Reference
 	public static final String LOWERCASE_MOD_ID = MOD_ID.toLowerCase();
 	public static final String MOD_NAME = "My Custom Ores";
 	public static final String FINGERPRINT = "@FINGERPRINT@";
-	public static final String VERSION = "1.7.10-0.0.1";
+	public static final String VERSION = "@VERSION@";
 	public static final String SERVER_PROXY_CLASS = "co.zerono.mco.proxy.ServerProxy";
 	public static final String CLIENT_PROXY_CLASS = "co.zerono.mco.proxy.ClientProxy";
 	public static final String GUI_FACTORY_CLASS = "co.zerono.mco.client.GuiFactory";
